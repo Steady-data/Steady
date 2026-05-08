3c241804f19a7560d8fbd986adca3a2b5f7231bf
Nof Shelly <nof@steady-data.com>
2026-05-08 17:03:56 +0300

    fix: use configurable:true in polyfill to allow module redefinition

--- scripts/registerWebModule.polyfill.js
+++ scripts/registerWebModule.polyfill.js
@@ -1,5 +1,6 @@
 // registerWebModule polyfill for expo-modules-core
 // Injected at the start of the Metro bundle to fix blank screen on web
+// Uses Object.defineProperty with configurable:true so modules can redefine it
 (function(){
   var d = globalThis.__d;
   if (d && !globalThis.__rwm) {
@@ -8,7 +9,12 @@
       d(function(g, r, i, a, m, exports, t) {
         factory(g, r, i, a, m, exports, t);
         if (exports && !exports.registerWebModule) {
-          exports.registerWebModule = function(cls) { return cls; };
+          Object.defineProperty(exports, 'registerWebModule', {
+            configurable: true,
+            writable: true,
+            enumerable: true,
+            value: function(cls) { return cls; }
+          });
         }
       }, id, deps);
     };
