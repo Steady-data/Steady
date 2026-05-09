9b528d1ed59a410d12d4774a8a9348a6da6f9200
Nof Shelly <nof@steady-data.com>
2026-05-09 15:04:30 +0300

    fix: use simple assignment in registerWebModule polyfill to prevent redefine conflict

--- scripts/registerWebModule.polyfill.js
+++ scripts/registerWebModule.polyfill.js
@@ -1,22 +1,17 @@
 // registerWebModule polyfill for expo-modules-core
 // Injected at the start of the Metro bundle to fix blank screen on web
-// Uses Object.defineProperty with configurable:true so modules can redefine it
+// Uses simple assignment so modules can freely redefine it with Object.defineProperty
 (function(){
-  var d = globalThis.__d;
-  if (d && !globalThis.__rwm) {
-    globalThis.__rwm = 1;
-    globalThis.__d = function(factory, id, deps) {
-      d(function(g, r, i, a, m, exports, t) {
-        factory(g, r, i, a, m, exports, t);
-        if (exports && !exports.registerWebModule) {
-          Object.defineProperty(exports, 'registerWebModule', {
-            configurable: true,
-            writable: true,
-            enumerable: true,
-            value: function(cls) { return cls; }
-          });
-        }
-      }, id, deps);
-    };
-  }
-})();
+    var d = globalThis.__d;
+    if (d && !globalThis.__rwm) {
+          globalThis.__rwm = 1;
+          globalThis.__d = function(factory, id, deps) {
+                  d(function(g, r, i, a, m, exports, t) {
+                            factory(g, r, i, a, m, exports, t);
+                            if (exports && !exports.registerWebModule) {
+                                        exports.registerWebModule = function(cls) { return cls; };
+                            }
+                  }, id, deps);
+          };
+    }
+}());
