71c146ad9568c159e340e7a1f19a413e2a649261
Nof Shelly <nof@steady-data.com>
2026-05-11 14:34:54 +0300

    fix: patch Object.defineProperty to prevent registerWebModule redefinition error

--- scripts/registerWebModule.polyfill.js
+++ scripts/registerWebModule.polyfill.js
@@ -1,17 +1,28 @@
 // registerWebModule polyfill for expo-modules-core
 // Injected at the start of the Metro bundle to fix blank screen on web
-// Uses simple assignment so modules can freely redefine it with Object.defineProperty
+// Wraps Object.defineProperty to ensure registerWebModule is always configurable,
+// preventing "Cannot redefine property: registerWebModule" TypeError
 (function(){
-    var d = globalThis.__d;
-    if (d && !globalThis.__rwm) {
-          globalThis.__rwm = 1;
-          globalThis.__d = function(factory, id, deps) {
-                  d(function(g, r, i, a, m, exports, t) {
-                            factory(g, r, i, a, m, exports, t);
-                            if (exports && !exports.registerWebModule) {
-                                        exports.registerWebModule = function(cls) { return cls; };
-                            }
-                  }, id, deps);
-          };
-    }
+        // Patch Object.defineProperty so registerWebModule is always configurable/writable
+     var origDefineProperty = Object.defineProperty;
+        Object.defineProperty = function(obj, prop, desc) {
+                    if (prop === 'registerWebModule' && desc && !desc.configurable) {
+                                    desc = Object.assign({}, desc, { configurable: true, writable: true });
+                    }
+                    return origDefineProperty.call(this, obj, prop, desc);
+        };
+
+     var d = globalThis.__d;
+        if (d && !globalThis.__rwm) {
+                    globalThis.__rwm = 1;
+                    globalThis.__d = function(factoryId, moduleFactory, deps) {
+                                    var factoryWrapper = function(global, required, module, exports) {
+                                                        if (!exports.registerWebModule) {
+                                                                                exports.registerWebModule = function(cls) { return cls; };
+                                                        }
+                                                        moduleFactory(global, required, module, exports);
+                                    };
+                                    d(factoryId, factoryWrapper, deps);
+                    };
+        }
 }());
