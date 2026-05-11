a59828d6dfaf77b09e2e2c48760a88d3102965cc
Nof Shelly <nof@steady-data.com>
2026-05-11 14:42:27 +0300

    fix: patch Object.defineProperty to prevent registerWebModule TypeError on web

--- scripts/registerWebModule.polyfill.js
+++ scripts/registerWebModule.polyfill.js
@@ -3,26 +3,26 @@
 // Wraps Object.defineProperty to ensure registerWebModule is always configurable,
 // preventing "Cannot redefine property: registerWebModule" TypeError
 (function(){
-        // Patch Object.defineProperty so registerWebModule is always configurable/writable
-     var origDefineProperty = Object.defineProperty;
-        Object.defineProperty = function(obj, prop, desc) {
-                    if (prop === 'registerWebModule' && desc && !desc.configurable) {
-                                    desc = Object.assign({}, desc, { configurable: true, writable: true });
-                    }
-                    return origDefineProperty.call(this, obj, prop, desc);
-        };
-
-     var d = globalThis.__d;
-        if (d && !globalThis.__rwm) {
-                    globalThis.__rwm = 1;
-                    globalThis.__d = function(factoryId, moduleFactory, deps) {
-                                    var factoryWrapper = function(global, required, module, exports) {
-                                                        if (!exports.registerWebModule) {
-                                                                                exports.registerWebModule = function(cls) { return cls; };
-                                                        }
-                                                        moduleFactory(global, required, module, exports);
-                                    };
-                                    d(factoryId, factoryWrapper, deps);
-                    };
+    // Patch Object.defineProperty so registerWebModule is always configurable/writable
+    var origDefineProperty = Object.defineProperty;
+    Object.defineProperty = function(obj, prop, desc) {
+        if (prop === 'registerWebModule' && desc && !desc.configurable) {
+            desc = Object.assign({}, desc, { configurable: true, writable: true });
         }
+        return origDefineProperty.call(this, obj, prop, desc);
+    };
+
+    var d = globalThis.__d;
+    if (d && !globalThis.__rwm) {
+        globalThis.__rwm = 1;
+        globalThis.__d = function(factoryId, moduleFactory, deps) {
+            var factoryWrapper = function(global, required, module, exports) {
+                if (!exports.registerWebModule) {
+                    exports.registerWebModule = function(cls) { return cls; };
+                }
+                moduleFactory(global, required, module, exports);
+            };
+            d(factoryId, factoryWrapper, deps);
+        };
+    }
 }());
